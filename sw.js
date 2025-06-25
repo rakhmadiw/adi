01043fe39f836a31b66c7f70ea78e5e55729fc9d
rakhmadiw <80022604+rakhmadiw@users.noreply.github.com>
2025-06-25 10:17:56 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('prompt-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
